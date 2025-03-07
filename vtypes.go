50b8d502709f187b11ecb29a103a6d2700b3e1b2
daved <daved@codemodus.com>
2025-03-07 15:25:26 -0800

    Make value creation at ptr lazy in hydrate

--- vtypes.go
+++ vtypes.go
@@ -45,14 +45,60 @@ func Hydrate(val any, raw string) error {
 		return NewError(NewHydrateError(err, val))
 	}
 
-	preppedVal, err := prepRef(val)
+	tmpVal, pointerChain, err := tempValue(val)
 	if err != nil {
 		return wrap(err)
 	}
 
-	switch v := preppedVal.(type) {
+	err = hydrateValue(tmpVal, raw)
+	if err != nil {
+		return wrap(err)
+	}
+
+	err = assignThroughChain(tmpVal, pointerChain)
+	if err != nil {
+		return wrap(err)
+	}
+
+	return nil
+}
+
+func tempValue(val any) (prepared any, pointerChain []reflect.Value, err error) {
+	v := reflect.ValueOf(val)
+	if v.Kind() != reflect.Pointer {
+		return nil, nil, ErrTypeUnsupported
+	}
+
+	// Collect all pointer levels
+	pointerChain = make([]reflect.Value, 0)
+	current := v
+
+	// Keep following pointers until we hit a non-pointer or nil at the deepest level
+	for current.Kind() == reflect.Pointer {
+		if current.IsNil() {
+			// Create new values for nil pointers
+			newVal := reflect.New(current.Type().Elem())
+			current.Set(newVal)
+		}
+		pointerChain = append(pointerChain, current)
+		current = current.Elem()
+	}
+
+	// We want to work with a single pointer to the final value
+	if len(pointerChain) < 1 {
+		return nil, nil, ErrTypeUnsupported
+	}
+
+	// The prepared value will be the last pointer in the chain
+	prepared = pointerChain[len(pointerChain)-1].Interface()
+	return prepared, pointerChain, nil
+}
+
+// hydrateValue handles the actual parsing and assignment to the prepared single-pointer value
+func hydrateValue(val any, raw string) error {
+	switch v := val.(type) {
 	case error:
-		return wrap(v)
+		return v
 
 	case *string:
 		*v = raw
@@ -60,141 +106,145 @@ func Hydrate(val any, raw string) error {
 	case *bool:
 		b, err := strconv.ParseBool(raw)
 		if err != nil {
-			return wrap(err)
+			return err
 		}
 		*v = b
 
 	case *int:
 		n, err := strconv.Atoi(raw)
 		if err != nil {
-			return wrap(err)
+			return err
 		}
 		*v = n
 
 	case *int64:
 		n, err := strconv.ParseInt(raw, 10, 0)
 		if err != nil {
-			return wrap(err)
+			return err
 		}
 		*v = n
 
 	case *int8:
 		n, err := strconv.ParseInt(raw, 10, 8)
 		if err != nil {
-			return wrap(err)
+			return err
 		}
 		*v = int8(n)
 
 	case *int16:
 		n, err := strconv.ParseInt(raw, 10, 16)
 		if err != nil {
-			return wrap(err)
+			return err
 		}
 		*v = int16(n)
 
 	case *int32:
 		n, err := strconv.ParseInt(raw, 10, 32)
 		if err != nil {
-			return wrap(err)
+			return err
 		}
 		*v = int32(n)
 
 	case *uint:
 		n, err := strconv.ParseUint(raw, 10, 0)
 		if err != nil {
-			return wrap(err)
+			return err
 		}
 		*v = uint(n)
 
 	case *uint64:
 		n, err := strconv.ParseUint(raw, 10, 0)
 		if err != nil {
-			return wrap(err)
+			return err
 		}
 		*v = n
 
 	case *uint8:
 		n, err := strconv.ParseUint(raw, 10, 8)
 		if err != nil {
-			return wrap(err)
+			return err
 		}
 		*v = uint8(n)
 
 	case *uint16:
 		n, err := strconv.ParseUint(raw, 10, 16)
 		if err != nil {
-			return wrap(err)
+			return err
 		}
 		*v = uint16(n)
 
 	case *uint32:
 		n, err := strconv.ParseUint(raw, 10, 32)
 		if err != nil {
-			return wrap(err)
+			return err
 		}
 		*v = uint32(n)
 
 	case *float64:
 		f, err := strconv.ParseFloat(raw, 64)
 		if err != nil {
-			return wrap(err)
+			return err
 		}
 		*v = f
 
 	case *float32:
 		f, err := strconv.ParseFloat(raw, 32)
 		if err != nil {
-			return wrap(err)
+			return err
 		}
 		*v = float32(f)
 
 	case *time.Duration:
 		d, err := time.ParseDuration(raw)
 		if err != nil {
-			return wrap(err)
+			return err
 		}
 		*v = d
 
 	case TextMarshalUnmarshaler:
 		if err := v.UnmarshalText([]byte(raw)); err != nil {
-			return wrap(err)
+			return err
 		}
 
 	case StringSetter:
 		if err := v.Set(raw); err != nil {
-			return wrap(err)
+			return err
 		}
 
 	case OnSetter:
 		if err := v.OnSet(raw); err != nil {
-			return wrap(err)
+			return err
 		}
 
 	default:
-		return wrap(ErrTypeUnsupported)
+		return ErrTypeUnsupported
 	}
 
 	return nil
 }
 
-func prepRef(val any) (any, error) {
-	vo := reflect.ValueOf(val)
-	if vo.Kind() != reflect.Pointer {
-		return nil, ErrTypeUnsupported
-	}
-	if vo.IsNil() {
-		return nil, ErrValueUnsupported
+// assignThroughChain propagates the value back through the pointer chain
+func assignThroughChain(prepared any, pointerChain []reflect.Value) error {
+	if len(pointerChain) == 0 {
+		return nil
 	}
 
-	el := vo.Elem()
-	if el.Kind() != reflect.Pointer {
-		return val, nil
-	}
-	if el.IsNil() {
-		el.Set(reflect.New(el.Type().Elem()))
+	preparedVal := reflect.ValueOf(prepared)
+	if preparedVal.Kind() != reflect.Pointer {
+		return ErrTypeUnsupported
 	}
 
-	return el.Interface(), nil
+	// Start with the actual value
+	currentVal := preparedVal.Elem()
+
+	// Work backwards through the pointer chain
+	for i := len(pointerChain) - 1; i >= 0; i-- {
+		pointer := pointerChain[i]
+		pointer.Elem().Set(currentVal)
+		currentVal = pointer
+	}
+
+	return nil
 }
 
 // ValueTypeName returns a "best effort" text representation of the value's
